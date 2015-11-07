d4bd729958f4225ffd7cfd57af604ff9054446ba
Seth Allen <seth.a@me.com>
2015-11-07 03:52:40 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecoelectronix::Application.config.secret_key_base = 'aed0a9c3acf935443ff0547b0d02421984f9ade3245959bdd24f80a46c23d4cd382f97eee45a05d18adb9388a946c8f11fbc45bac0dd278d812d540d4df7739c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ecoelectronix::Application.config.session_store :cookie_store, key: '_ecoelectronix_session'
