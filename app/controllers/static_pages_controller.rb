dbfa097c123b7de650f4152b43637d9719e51c5d
Seth Allen <seth.a@me.com>
2016-01-14 23:04:19 +0000

    fixing CSRF Auth Token issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:post]
+  skip_before_filter  :verify_authenticity_token
 
   def index
   end
